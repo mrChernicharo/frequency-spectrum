6805b1548fa4754c2e08df63192d4078fc39513f
Felipe Chernicharo <string7dev@gmail.com>
2022-09-27 23:06:18 -0400

    complete

--- src/App.tsx
+++ src/App.tsx
@@ -9,22 +9,31 @@ import { arc, interpolateSinebow, interpolateInferno } from 'd3';
 
 const arcBuilder = arc();
 
-const RadialGraph: Component = () => {
-	const paths = createMemo(() => {
+const RadialGraph: Component<{
+	color: (value: number) => string;
+	scale: number;
+}> = props => {
+	const computed = createMemo(() => {
 		const data = rawData();
+
+		const total = data.reduce((acc, v) => acc + v, 0);
+
+		const highCount = data.filter(d => d > 32).length;
+		const intensity = highCount / data.length;
 
 		const paths: {
 			path: string;
 			color: string;
 		}[] = [];
 
-		const range = 1.8;
+		const range = 1.0 + intensity;
 		const rangeInRadians = range * Math.PI;
 		const startAngle = -(rangeInRadians / 2);
-		const angle = rangeInRadians / data.length;
 		let currAngle = startAngle;
 
 		for (const d of data) {
+			const angle = rangeInRadians * (d / total);
+
 			const path = arcBuilder({
 				innerRadius: 50 - ((d + 10) / 255) * 35,
 				outerRadius: 50 + ((d + 10) / 255) * 35,
@@ -32,16 +41,18 @@ const RadialGraph: Component = () => {
 				endAngle: currAngle + angle,
 			})!;
 
-			paths.push({ path, color: interpolateSinebow(d / 255) });
+			paths.push({ path, color: props.color(d / 255) });
 			currAngle += angle;
 		}
 
-		return paths;
+		return { paths, intensity };
 	});
 
 	return (
-		<g>
-			<For each={paths()}>{p => <path d={p.path} fill={p.color} />}</For>
+		<g transform={`scale(${computed().intensity * props.scale + 1})`}>
+			<For each={computed().paths}>
+				{p => <path d={p.path} fill={p.color} />}
+			</For>
 		</g>
 	);
 };
@@ -78,10 +89,11 @@ const App: Component = () => {
 			<svg
 				width="100%"
 				height="100%"
-				viewBox="-100 -100 200 200"
+				viewBox="-100 -150 200 200"
 				preserveAspectRatio="xMidYMid meet"
 			>
-				<RadialGraph />
+				<RadialGraph color={interpolateSinebow} scale={1.5} />
+				<RadialGraph color={interpolateInferno} scale={1} />
 			</svg>
 
 			<ProgressIndicator />
